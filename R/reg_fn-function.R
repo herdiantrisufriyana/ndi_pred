b48021c3dcb789ecd6b87c77b0f665e17131d1c2
herdiantrisufriyana <herdiantrisufriyana@gmail.com>
2024-11-10 16:30:19 +0800

    Inverse survive value and change Surv type to counting.

--- R/reg_fn-function.R
+++ R/reg_fn-function.R
@@ -1,13 +1,32 @@
 reg_fn <- function(formula, data){
+  
   if("t" %in% colnames(data)){
+    
+    if(as.character(formula)[2] == "survive"){
+      data <-
+        data |>
+        mutate(survive = ifelse(t == max(t), survive, 1)) |>
+        mutate(survive = 1 - survive)
+    }
+    
+    data <-
+      data |>
+      mutate(tstart = t - 1) |>
+      rename(tstop = t)
+    
     data <-
       data |>
       rename_at(as.character(formula)[2], \(x) "outcome") |>
-      mutate(outcome = Surv(t, outcome)) |>
+      mutate(
+        outcome =
+          Surv(time = tstart, time2 = tstop, event = outcome, type = "counting")
+      ) |>
       rename_at("outcome", \(x) as.character(formula)[2])
     
     coxph(formula, data, id = id)
+    
   }else{
     glm(formula, binomial(), data)
   }
+  
 }
